80859ea45f49d1013ad940affa0e2a320831e80f
RomanGodOfPHP <cuclovod5611@gmail.com>
2017-07-10 00:18:53 +0300

    Версия 2!)

--- index.php
+++ index.php
@@ -10,15 +10,40 @@
 <body>
 <div class="main-class">
 	<header class="header"><img src="img/ac-dc-logo.png">
-		<div class="navigation">
+		<div id="mainmenu">
 			<ul>
-				<li><a href="band.php">Группа</a></li>
-				<li><a href="concerts.php">Концертный тур</a></li>
-				<li><a href="gallery.php">Галлерея</a></li>
-				<li><a href="cotacts.php">Контакты</a></li>
+				<li><a href="band.php" style="border-radius: 8px 8px 0px 0px;" class="radius">Группа</a></li>
+				<li><a href="concerts.php" style="border-radius: 8px 8px 0px 0px;" class="radius">Концертный тур</a></li>
+				<li><a href="gallery.php" style="border-radius: 8px 8px 0px 0px;" class="radius">Галлерея</a></li>
+				<li><a href="cotacts.php" style="border-radius: 8px 8px 0px 0px;" class="radius">Контакты</a></li>
 			</ul>
 		</div>
 	</header>
+	<div class="content">
+		<H1>AC/DC - это круто!</H1>
+		<p>AC/DC (сокр. от англ. alternating current/direct current — переменный/постоянный ток)[1] — 
+		австралийская рок-группа, сформированная в Сиднее в ноябре 1973 года выходцами из Шотландии, братьями Малькольмом 
+		и Ангусом Янгами.</p>
+		<p>Вместе с такими группами как Led Zeppelin, Deep Purple, Queen, Iron Maiden, Scorpions, Black Sabbath, Uriah Heep, 
+		Judas Priest и Motorhead AC/DC часто рассматриваются как пионеры хард-рока и хеви-метала.[2][3] Сами же музыканты 
+		классифицировали свою музыку как рок-н-ролл[4], поскольку в её основе лежит ритм-н-блюз с сильно искажённым звучанием 
+		ритмической и соло-гитар.</p>
+		<p>Коллектив прошёл через несколько изменений составов, прежде чем в 1975 году был издан первый альбом группы, 
+		High Voltage. Состав группы оставался неизменным, пока бас-гитарист Марк Эванс[en] (англ. Mark Evans) не был замещён 
+		Клиффом Уильямсом (англ. Cliff Williams) в 1977 году. 19 февраля 1980 года от сильного алкогольного опьянения 
+		умер вокалист и автор песен коллектива Бон Скотт. Группа имела все шансы распасться, однако замена Скотту 
+		была найдена в лице бывшего вокалиста группы Geordie Брайана Джонсона. В этом же году группа записала свой 
+		самый продаваемый альбом, Back in Black.</p>
+		<p>Коллектив продал свыше 200 миллионов копий альбомов по всему миру[5][6], включая 72 миллиона в США[7]. 
+		Самый успешный альбом, Back in Black, был продан в количестве более 22 миллионов в США[8] и более 42 миллионов 
+		за их пределами[9]. В целом AC/DC является самой успешной и известной рок-группой из Австралии и одной из 
+		популярнейших в мире.</p>
+	</div>
+	<footer class="footer">
+		<div class="footer-menu">
+	
+		</div>
+	</footer>
 </div>
 </body>
 </html>
