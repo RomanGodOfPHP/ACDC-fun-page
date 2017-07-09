37aa2f0e08ab318c1930b4eec58c285b9c3417bd
RomanGodOfPHP <cuclovod5611@gmail.com>
2017-07-09 23:04:50 +0300

    Создана первая версия!

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
+<title>AC/DC фан-страница</title>
+<meta name="discription" content="фан-страница группы AC/DC">
+<meta name="keywords" content="AC/DC, band, group, concerts, studio, audio, music, музыка, группа, концерты">
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="main-class">
+	<header class="header"><img src="img/ac-dc-logo.png">
+		<div class="navigation">
+			<ul>
+				<li><a href="band.php">Группа</a></li>
+				<li><a href="concerts.php">Концертный тур</a></li>
+				<li><a href="gallery.php">Галлерея</a></li>
+				<li><a href="cotacts.php">Контакты</a></li>
+			</ul>
+		</div>
+	</header>
+</div>
+</body>
+</html>
